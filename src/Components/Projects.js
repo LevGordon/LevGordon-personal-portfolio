9c9eff7c3f9d4ba6c99cd7327c365daf0ebee6f8
Lev_Gordon <lev.gordonsa@gmail.com>
2022-10-31 08:58:23 -0700

    filled out info, uploaded project images and made little changes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,39 +1,45 @@
 import React from 'react'
 import { Col, Container, Nav, Row, Tab } from 'react-bootstrap'
 import ProjectCard from './ProjectCard'
+import DR from '../assets/imgs/projects/DR.png'
+import PP from '../assets/imgs/projects/PP.png'
+import PT from '../assets/imgs/projects/PT.png'
+import RM from '../assets/imgs/projects/RM.png'
+import TRVL from '../assets/imgs/projects/TRVL.png'
+import WLM from '../assets/imgs/projects/WLM.png'
 
 function Projects() {
 
     const projects = [
-        { title: 'Restaurant Reservation App',
-        description: 'A full-stack application built with Node, Express, Knex and React',
-        imgUrl: ''
-
-        },
+         {  
+            title: 'Restaurant Reservation App',
+            description: 'A full-stack application built with Node, Express, Knex and React that allows restaurant owner/staff to create/store/manage table reservations',
+            imgUrl: PT
+         },
          {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: 'projImg2',
+            title: "Red Morrow Band",
+            description: "Frontend multi-page website made with React to showcase a rock band and allows their fans to access music/info about them",
+            imgUrl: RM,
           },
           {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: 'projImg3',
+            title: "TRVL Agency",
+            description: "A frontend multi-page website that displays travel destinations and offers travelling services.",
+            imgUrl: TRVL,
           },
           {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: 'projImg1',
+            title: "We Love Movies",
+            description: "Backend API / validation / functionality with Node, Express, Knex and PostgreSQL for a website like netflix",
+            imgUrl: WLM,
           },
           {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: 'projImg2',
+            title: "Personal Portfolio",
+            description: "Lev Gordon's personal potfolio with React (you are looking at it)",
+            imgUrl: PP,
           },
           {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: 'projImg3',
+            title: "Decoder Ring",
+            description: "A frontend JS application that allows two parties to encode and later decode messages using 3 different encryption techniques",
+            imgUrl: DR,
           },
     ]
 
@@ -74,10 +80,10 @@ function Projects() {
 
                         </Tab.Pane>
                         <Tab.Pane eventKey='second'>
-                            <p>Lorem Ipsum</p>
+                            <p>More coming soon</p>
                         </Tab.Pane>
                         <Tab.Pane eventKey='third'>
-                            <p>Lorem Ipsum</p>
+                            <p>More coming soon</p>
                         </Tab.Pane>
                     </Tab.Content>
                     </Tab.Container>
